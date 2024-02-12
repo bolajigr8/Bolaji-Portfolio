df82364f2162b75f3b8da59cf87cc0ba266b5293
Bolaji Michael Adebayo <144087782+bolajigr8@users.noreply.github.com>
2024-02-12 16:23:06 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+const cl = console.log.bind(console);
+
 const navbar = document.querySelector("#nav");
 const navBtn = document.querySelector("#nav-btn");
 const closeBtn = document.querySelector("#close-btn");
@@ -13,7 +15,7 @@ window.addEventListener("scroll", function () {
 });
 // show sidebar
 navBtn.addEventListener("click", function () {
-  sidebar.classList.add("show-sidebar");
+  sidebar.classList.toggle("show-sidebar");
 });
 closeBtn.addEventListener("click", function () {
   sidebar.classList.remove("show-sidebar");
@@ -22,9 +24,35 @@ closeBtn.addEventListener("click", function () {
 date.innerHTML = new Date().getFullYear();
 
 // source code
-const sourceCode = document.getElementById("sourceCode");
 
-function signal() {
-  alert("Not yet uploaded");
-}
-sourceCode.addEventListener("click", signal);
+const sCode = document.querySelectorAll("#sCode");
+cl(sCode);
+sCode.forEach(function (e) {
+  e.addEventListener("click", function () {
+    alert("Not yet uploaded");
+  });
+});
+
+// sCode.addEventListener("click", function signal() {
+//   alert("Not yet uploaded");
+// });
+
+// top link
+
+const topLink = document.querySelector(".top-link");
+window.addEventListener("scroll", function () {
+  const scrollHeight = window.pageYOffset;
+  if (scrollHeight > 800) {
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
+  }
+});
+
+topLink.addEventListener("click", function (e) {
+  e.preventDefault();
+  window.scrollTo({
+    left: 0,
+    top: 0,
+  });
+});
